9563a2884b3e8f54e06ca840381aa723450f851d
Braulio Solorio <soloriobraulio03@gmail.com>
2024-04-21 23:47:18 -0600

    about sends you to game view

--- clientUI/views/about/about.js
+++ clientUI/views/about/about.js
@@ -1,8 +1,13 @@
-const usernameTxt = document.getElementById("username")
-const creditsTxt = document.getElementById("userCredits")
+const startGame = document.getElementById("startGame");
+const usernameTxt = document.getElementById("username");
+const creditsTxt = document.getElementById("userCredits");
 
 window.onload = async () => {
     const user = JSON.parse(await window.userData.getUserData());
     usernameTxt.innerHTML = user.username;
     creditsTxt.innerHTML = user.credits;
 }
+
+startGame.addEventListener("click", () => {
+    window.location.replace("./game.html")
+})
